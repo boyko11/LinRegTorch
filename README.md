b5b978bfed891eaf9ffc78b7077432249988f125
Boyko <boyko11@gmail.com>
2020-05-09 13:35:38 -0400

    Linear Regression with PyTorch

---
+++ README.md
@@ -0,0 +1,35 @@
+Linear Regresssion with PyTorch
+
+## Dataset
+The simple cengage systolic blood pressure dataset:  
+https://college.cengage.com/mathematics/brase/understandable_statistics/7e/students/datasets/mlr/frames/frame.html
+
+## Purpose
+Since I implemented linear regression from scratch,  
+this is to see if PyTorch can make it easier
+
+#### Run verified with python 3.7 and numpy 1.18.1
+python lin_reg.py
+
+## Loss Per PyTorch Step (Learning Curve)
+![Loss per Tensorflow Step](plots/learning_curve.png)
+
+## Predicted vs Actual
+![Predicted vs Actual](plots/predicted_vs_actual.png)
+
+## Prediction Results over the Training set
+
+|Age|Weight|Actual Blood Pressure|Predicted Blood Pressure|Loss|
+|:-------:|:---:|:--------------------:|:---------------------:|:----------:|
+|52.0|173.0|132.0|128.0|4.0|
+|59.0|184.0|143.0|140.0|3.0|
+|67.0|194.0|153.0|153.0|0.0|
+|73.0|211.0|162.0|166.0|4.0|
+|64.0|196.0|154.0|150.0|4.0|
+|74.0|220.0|168.0|171.0|3.0|
+|54.0|188.0|137.0|137.0|0.0|
+|61.0|188.0|149.0|144.0|5.0|
+|65.0|207.0|159.0|156.0|3.0|
+|46.0|167.0|128.0|119.0|9.0|
+|72.0|217.0|166.0|168.0|2.0|
+
---
+++ service/plot_service.py
@@ -0,0 +1,73 @@
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as plt3d
+
+
+class PlotService:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def plot3d_scatter(data, labels, marker='o', title=''):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], marker=marker)
+
+        ax.set_xlabel(labels[0])
+        ax.set_ylabel(labels[1])
+        ax.set_zlabel(labels[2])
+
+        plt.title(title)
+
+        plt.show()
+
+    @staticmethod
+    def plot3d_scatter_compare(feature_data, labels_data, predictions, labels=['', '', ''], marker1='o', marker2='x', color1=None, color2=None, title=''):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(feature_data[:, 0], feature_data[:, 1], labels_data, marker=marker1, label="Actual")
+        ax.scatter(feature_data[:, 0], feature_data[:, 1], predictions, marker=marker2, label="Projected")
+
+        ax.set_xlabel(labels[0])
+        ax.set_ylabel(labels[1])
+        ax.set_zlabel(labels[2])
+        ax.legend()
+
+        plt.title(title)
+
+        plt.show()
+
+    def plot3d_line(x, y, z, labels, title=''):
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+
+        ax.plot(x, y, zs=z, label='Linear Model')
+
+        ax.set_xlabel(labels[0])
+        ax.set_ylabel(labels[1])
+        ax.set_zlabel(labels[2])
+
+        plt.title(title)
+
+        plt.show()
+
+    @staticmethod
+    def plot_line(x, y, x_label, y_label, title='', ylim=None):
+
+        plt.figure()
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        if ylim is not None:
+            plt.ylim(ylim)
+
+        plt.grid()
+
+        plt.plot(x, y)
+
+        plt.show()
